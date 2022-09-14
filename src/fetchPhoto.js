5d6e5573ebab24012574652c5cca17d6c6a152c6
Volodymyr Husak <volodyaog2907@gmail.com>
2022-09-14 22:51:32 +0300

    add axios

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -1,42 +1,23 @@
-// export default function fetchCountries(name) {
-//   return fetch(
-//     `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
-//   )
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     })
-//     .catch(error => {
-//       console.warn(error);
-//     });
+// export default async function fetchPhoto(userSearch) {
+//   const response = await fetch(
+//     `https://pixabay.com/api/?key=29882224-53e6cb6eb5c61ad27904c20c4&q=${userSearch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&`
+//   );
+//   const result = await response.json();
+//   return result;
 // }
 
-export default async function fetchPhoto(userSearch) {
-  // return fetch(
-  //   // `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
-  //   `https://pixabay.com/api/?key=29882224-53e6cb6eb5c61ad27904c20c4&q=yellow+flowers&image_type=photo`
-  // )
-  // .then(response => {
-  //   if (!response.ok) {
-  //     throw new Error(response.status);
-  //   }
-  //   return response.json();
-  // })
-  // .catch(error => {
-  //   console.warn(error);
-  // });
-  // userSearch
-  const response = await fetch(
-    `https://pixabay.com/api/?key=29882224-53e6cb6eb5c61ad27904c20c4&q=${userSearch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&`
-  );
-  const result = await response.json();
-  return result;
-}
+const axios = require('axios');
 
-// const fetchUsers = async () => {
-//   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-//   const users = await response.json();
-//   return users;
-// };
+export default async function fetchPhoto(userSearch) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=29882224-53e6cb6eb5c61ad27904c20c4&q=${userSearch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&`
+    );
+    // console.log(response);
+    const result = await response;
+    return result;
+  } catch (error) {
+    // console.error(error);
+    console.log(error);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,116 +1,8 @@
-// import './css/styles.css';
-// import Notiflix from 'notiflix';
-// import fetchCountries from './fetchCountries.js';
-// // import countryListMarkup from './countryListMarkup';
-
-// import debounce from 'lodash.debounce';
-
-// const DEBOUNCE_DELAY = 300;
-
-// const refs = {
-//   inputEl: document.querySelector('#search-box'),
-//   countryListEl: document.querySelector('.country-list'),
-//   countryInfoEl: document.querySelector('.country-info'),
-// };
-// const { inputEl, countryListEl, countryInfoEl } = refs;
-
-// inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
-
-// function onInput(event) {
-//   let inputValue = event.target.value.trim();
-//   if (inputValue === '') {
-//     deleteCountryMarkup();
-//     return;
-//   }
-
-//   const country = inputValue;
-
-//   fetchCountries(country)
-//     .then(country => {
-//       // console.log(country);
-//       renderCountryMarkup(country);
-//     })
-//     .catch(error => {
-//       // console.warn(error);
-//       deleteCountryMarkup();
-//       Notiflix.Notify.failure('Oops, there is no country with that name.');
-//     });
-// }
-
-// function deleteCountryMarkup() {
-//   let itemCountryEl = document.querySelectorAll('.item-country');
-//   let countryInfoItemEl = document.querySelector('.country-info-item');
-
-//   itemCountryEl?.forEach(item => item.remove());
-//   countryInfoItemEl?.remove();
-// }
-
-// function renderCountryMarkup(country) {
-//   // console.log(country.length);
-//   if (country.length > 10) {
-//     deleteCountryMarkup();
-
-//     return Notiflix.Notify.info(
-//       'Too many matches found. Please enter a more specific name.'
-//     );
-//   }
-
-//   if (country.length === 1) {
-//     deleteCountryMarkup();
-
-//     const countryObj = country[0];
-
-//     // const countryLanguagesArr = Object.values(countryObj.languages);
-//     let countryLanguagesArr = [];
-
-//     const countryLanguagesArrObj = countryObj.languages;
-
-//     countryLanguagesArrObj.map(countryLanguage =>
-//       countryLanguagesArr.push(countryLanguage.name)
-//     );
-
-//     const markupInfo = `<div class="country-info-item">
-//        <div class="country-flag-container">
-//            <img class="country-info__flags" src="${countryObj.flags.svg}"
-//                alt="flags ${countryObj.name}" width=30 >
-//           <p class="country-info__text">${countryObj.name}</p>
-//        </div>
-
-//      <ul class="country-info__list">
-//          <li class="country-info__item"><span class="country-info__bold-text">Capital:</span> ${countryObj.capital}</li>
-//          <li class="country-info__item"><span class="country-info__bold-text">Population:</span> ${countryObj.population}</li>
-//          <li class="country-info__item"><span class="country-info__bold-text">Languages:</span> ${countryLanguagesArr}</li>
-//      </ul>
-//      </div>`;
-
-//     return countryInfoEl.insertAdjacentHTML('beforeend', markupInfo);
-//   }
-
-//   deleteCountryMarkup();
-
-//   const markupList = country
-//     .map(
-//       count =>
-//         `<li class="item-country">
-//             <img class="country-flag" src="${count.flags.svg}"
-//                alt="flags ${count.name}" width=30>
-//             <p class="country-list__text">${count.name}</p>
-//       </li>`
-//     )
-//     .join('');
-
-//   return countryListEl.insertAdjacentHTML('beforeend', markupList);
-// }
-
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import fetchPhoto from './fetchPhoto.js';
-// Описаний в документації
+
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // import debounce from 'lodash.debounce';
@@ -156,17 +48,11 @@ function deleteCountryMarkup() {
   let photoCardEl = document.querySelectorAll('.photo-card');
   photoCardEl?.forEach(element => element.remove());
 }
-// function deleteCountryMarkup() {
-//   let itemCountryEl = document.querySelectorAll('.item-country');
-//   let countryInfoItemEl = document.querySelector('.country-info-item');
-
-//   itemCountryEl?.forEach(item => item.remove());
-//   countryInfoItemEl?.remove();
-// }
 
 function renderPhotoMarkup(search) {
-  const hitsArr = search.hits;
-  // console.log(hitsArr);
+  console.log(search.data);
+  const hitsArr = search.data.hits;
+  console.log(hitsArr);
   if (!hitsArr.length) {
     return Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
@@ -178,31 +64,42 @@ function renderPhotoMarkup(search) {
 
   const markupCard = hitsArr
     .map(
-      hit => `<div class="photo-card">
-  <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" width=300 height=200/>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b><br>${hit.likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b><br>${hit.views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b><br>${hit.comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b><br>${hit.downloads}
-    </p>
-  </div>
-</div>`
+      hit => `      
+      <a class="photo-link" href="${hit.largeImageURL}">
+        <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" width=300 height=200/>
+      </a>`
     )
     .join('');
 
   return galleryEl.insertAdjacentHTML('beforeend', markupCard);
 }
 
-new SimpleLightbox('.gallery', {
+new SimpleLightbox('.gallery a', {
   /* options */
   captionsData: 'alt',
   captionDelay: 250,
 });
+// var gallery = $('.gallery a').simpleLightbox();
+
+// gallery.next(); // Next Image
+
+// `
+//       <a class="photo-link" href="${hit.largeImageURL}">
+//       <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" width=300 height=200/>
+//       </a>
+//       <div class="photo-card">
+//     <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b><br>${hit.likes}
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b><br>${hit.views}
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b><br>${hit.comments}
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b><br>${hit.downloads}
+//     </p>
+//   </div>
+// </div>`;
